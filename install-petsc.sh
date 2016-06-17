d60206b466daa60337f585ae0e24bff3b5d4dd96
Marco Zocca <ocramz@users.noreply.github.com>
2016-06-17 20:29:44 +0200

    Create install-petsc.sh

---
+++ install-petsc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export PETSC_VERSION=3.7.2
+
+
+# # env. variables
+
+export PETSC_DIR=/opt/petsc-$PETSC_VERSION
+export PETSC_ARCH=arch-linux2-c-debug
+
+# # # Update APT and install dependencies and tools
+apt-get update && apt-get upgrade -y && apt-get install -y --no-install-recommends \
+                       make \
+                       gcc gfortran wget curl unzip python pkg-config build-essential \
+                       valgrind \
+		       openssh-client openssh-server
+
+
+# # Download and extract PETSc.
+cd /opt
+wget --no-verbose http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-$PETSC_VERSION.tar.gz && \
+    gunzip -c petsc-lite-$PETSC_VERSION.tar.gz | tar -xof -
+
+cd $PETSC_DIR
+
+# # # Configure and build PETSc 
+
+echo "\n=== Configuring without batch mode\n"
+
+./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --download-mpich &&\
+    make all && \
+    make test
