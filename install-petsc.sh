19501332238bc1df770b0956c66864b8fbe98971
ocramz <zocca.marco@gmail.com>
2016-06-17 22:14:40 +0200

    typos

--- install-petsc.sh
+++ install-petsc.sh
@@ -5,14 +5,17 @@ apt-get update && apt-get install -y --no-install-recommends \
 			  wget curl unzip python pkg-config build-essential \
 		          openssh-client openssh-server
 
+cd /opt
 
 # # Download and extract PETSc.
 wget --no-verbose http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-$PETSC_VERSION.tar.gz && \
     gunzip -c petsc-lite-$PETSC_VERSION.tar.gz | tar -xof -
 
-# # # Configure and build PETSc 
+# # # Configure and build PETSc
 
-echo "\n=== Configuring without batch mode\n"
+cd $PETSC_DIR
+
+echo "=== Configuring without batch mode"
 
 ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --download-mpich &&\
     make all && \
